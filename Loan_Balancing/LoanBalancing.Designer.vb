3dc6b9c6ed3fe04fd8e1fa0b4f43bbe39441cc3f
Ian Stabler <Ian@investandfund.com>
2020-02-28 17:09:08 +0000

    Add project files.

---
+++ Loan_Balancing/LoanBalancing.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.msgMessage = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.LoanID = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.LoanName = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.FixedRate = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.MaxLoanAmount = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.LoanHoldingID = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Rate = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NumUnits = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.SumBalance = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.smryLoanID = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.smryLoanAmt = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.smryLoanTotal = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Difference = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(12, 12)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(158, 23)
+        Me.btnGo.TabIndex = 0
+        Me.btnGo.Text = "Perform Loan Balancing"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'msgMessage
+        '
+        Me.msgMessage.AutoSize = True
+        Me.msgMessage.Location = New System.Drawing.Point(294, 17)
+        Me.msgMessage.Name = "msgMessage"
+        Me.msgMessage.Size = New System.Drawing.Size(53, 13)
+        Me.msgMessage.TabIndex = 1
+        Me.msgMessage.Text = "Message:"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.LoanID, Me.LoanName, Me.FixedRate, Me.MaxLoanAmount, Me.LoanHoldingID, Me.Rate, Me.NumUnits, Me.SumBalance})
+        Me.DataGridView1.Location = New System.Drawing.Point(457, 51)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(1046, 813)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.smryLoanID, Me.smryLoanAmt, Me.smryLoanTotal, Me.Difference})
+        Me.DataGridView2.Location = New System.Drawing.Point(13, 51)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.Size = New System.Drawing.Size(438, 813)
+        Me.DataGridView2.TabIndex = 3
+        '
+        'LoanID
+        '
+        Me.LoanID.HeaderText = "LoanID"
+        Me.LoanID.Name = "LoanID"
+        Me.LoanID.Width = 70
+        '
+        'LoanName
+        '
+        Me.LoanName.HeaderText = "LoanName"
+        Me.LoanName.Name = "LoanName"
+        Me.LoanName.Width = 300
+        '
+        'FixedRate
+        '
+        Me.FixedRate.HeaderText = "FixedRate"
+        Me.FixedRate.Name = "FixedRate"
+        '
+        'MaxLoanAmount
+        '
+        Me.MaxLoanAmount.HeaderText = "MaxLoanAmount"
+        Me.MaxLoanAmount.Name = "MaxLoanAmount"
+        '
+        'LoanHoldingID
+        '
+        Me.LoanHoldingID.HeaderText = "LoanHoldingID"
+        Me.LoanHoldingID.Name = "LoanHoldingID"
+        '
+        'Rate
+        '
+        Me.Rate.HeaderText = "Rate"
+        Me.Rate.Name = "Rate"
+        '
+        'NumUnits
+        '
+        Me.NumUnits.HeaderText = "NumUnits"
+        Me.NumUnits.Name = "NumUnits"
+        '
+        'SumBalance
+        '
+        Me.SumBalance.HeaderText = "SumBalance"
+        Me.SumBalance.Name = "SumBalance"
+        '
+        'smryLoanID
+        '
+        Me.smryLoanID.HeaderText = "LoanID"
+        Me.smryLoanID.Name = "smryLoanID"
+        Me.smryLoanID.Width = 70
+        '
+        'smryLoanAmt
+        '
+        Me.smryLoanAmt.HeaderText = "LoanAmount"
+        Me.smryLoanAmt.Name = "smryLoanAmt"
+        '
+        'smryLoanTotal
+        '
+        Me.smryLoanTotal.HeaderText = "LoanTotal"
+        Me.smryLoanTotal.Name = "smryLoanTotal"
+        '
+        'Difference
+        '
+        Me.Difference.HeaderText = "Difference"
+        Me.Difference.Name = "Difference"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1517, 876)
+        Me.Controls.Add(Me.DataGridView2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.msgMessage)
+        Me.Controls.Add(Me.btnGo)
+        Me.Name = "Form1"
+        Me.Text = "Loan Balancing Report"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnGo As Button
+    Friend WithEvents msgMessage As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents LoanID As DataGridViewTextBoxColumn
+    Friend WithEvents LoanName As DataGridViewTextBoxColumn
+    Friend WithEvents FixedRate As DataGridViewTextBoxColumn
+    Friend WithEvents MaxLoanAmount As DataGridViewTextBoxColumn
+    Friend WithEvents LoanHoldingID As DataGridViewTextBoxColumn
+    Friend WithEvents Rate As DataGridViewTextBoxColumn
+    Friend WithEvents NumUnits As DataGridViewTextBoxColumn
+    Friend WithEvents SumBalance As DataGridViewTextBoxColumn
+    Friend WithEvents smryLoanID As DataGridViewTextBoxColumn
+    Friend WithEvents smryLoanAmt As DataGridViewTextBoxColumn
+    Friend WithEvents smryLoanTotal As DataGridViewTextBoxColumn
+    Friend WithEvents Difference As DataGridViewTextBoxColumn
+End Class
---
+++ Loan_Balancing/LoanBalancing.vb
@@ -0,0 +1,302 @@
+﻿Imports System.Globalization
+Imports System.Configuration
+Imports System.Collections.Specialized
+Imports System.IO
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Net.Mail
+
+Module LoanBalancing
+
+    Public dsLoans As DataSet
+    Public dsHoldings As DataSet
+
+    Private Function ExecuteIT() As String
+        Dim MySQL, strConn, sHTML, bHTML, sUsers As String
+        Dim MyConn As FirebirdSql.Data.FirebirdClient.FbConnection
+        Dim Cmd As FirebirdSql.Data.FirebirdClient.FbCommand
+        Dim Adaptor As FirebirdSql.Data.FirebirdClient.FbDataAdapter
+        Dim dr, dr1 As DataRow
+        sUsers = ConfigurationManager.AppSettings("EmailList")
+
+
+
+        strConn = ConfigurationManager.ConnectionStrings("FBConnectionString").ConnectionString
+        MyConn = New FirebirdSql.Data.FirebirdClient.FbConnection(strConn)
+        MyConn.Open()
+
+        MySQL = "Select  l.loanid, l.business_name, l.fixed_rate, l.MAXLOANAMOUNT
+                	From Loans l
+    	            Where  l.LOANSTATUS in (2, 7)
+                        and l.isactive = 0 
+                        order by  l.loanid"
+
+        dsLoans = New DataSet
+        Adaptor = New FirebirdSql.Data.FirebirdClient.FbDataAdapter(MySQL, MyConn)
+
+        Adaptor.Fill(dsLoans)
+
+        MyConn.Close()
+
+        Dim errorcount As Integer = Nothing
+
+
+        Dim iLoanCounter As Integer = dsLoans.Tables(0).Rows.Count
+
+        For i = 0 To iLoanCounter - 1
+            dr = dsLoans.Tables(0).Rows(i)
+            Dim loanid As Integer = dr("LoanID")
+
+            Dim loanname As String = dr("Business_Name")
+            Dim fixedrate As Integer = dr("Fixed_Rate")
+            Dim maxloanamount As Integer = dr("maxloanamount")
+            Dim smryLoanID, smryloanamount, smrytotal As Integer
+            smryLoanID = loanid
+            smryloanamount = maxloanamount
+
+            MyConn.Open()
+
+            MySQL = "Select  h.loan_holdings_id, h.rate, h.num_units, sum(b.num_units) as sumbalance
+                    From loan_holdings h, lh_balances b
+                    Where  h.loanid = " & loanid &
+                      "  and h.loan_holdings_id = b.lh_id
+                        and h.isactive = 0 
+                        group by h.loan_holdings_id,  h.rate,  h.num_units
+                        order by  h.loan_holdings_id"
+
+
+            dsHoldings = New DataSet
+            Adaptor = New FirebirdSql.Data.FirebirdClient.FbDataAdapter(MySQL, MyConn)
+
+            Adaptor.Fill(dsHoldings)
+
+            MyConn.Close()
+
+            Dim iHoldingCounter As Integer = dsHoldings.Tables(0).Rows.Count
+            Dim lhbalance As Integer = 0
+
+            For j = 0 To iHoldingCounter - 1
+                dr1 = dsHoldings.Tables(0).Rows(j)
+                Dim LHID As Integer = dr1("Loan_holdings_ID")
+                Dim rate As Integer = dr1("rate")
+                Dim numunits As Integer = dr1("num_units")
+                Dim sumbalance As Integer = dr1("sumbalance")
+                lhbalance += sumbalance
+
+
+
+                'Dim row As String() = New String() {loanid, loanname, fixedrate, maxloanamount, LHID, rate, numunits, sumbalance}
+                'DataGridView1.Rows.Add(row)
+
+
+            Next j
+
+            smrytotal = lhbalance
+            Dim Accepted As Integer = 0
+            Dim Difference As Integer = smryloanamount - smrytotal
+            'read the entries in the config file to establish if there is still debris on the database for this loan 
+            Try
+                Dim llist As String = "l" & loanid
+                Dim laccepted As String = ConfigurationManager.AppSettings(llist)
+                If laccepted IsNot Nothing Then
+                    Accepted = laccepted
+                End If
+            Catch ex As Exception
+
+            End Try
+
+            Dim Balance As Integer = Difference - Accepted
+            errorcount = errorcount + Balance
+
+            'Dim row2 As String() = New String() {smryLoanID, smryloanamount, smrytotal, Difference}
+            'DataGridView2.Rows.Add(row2)
+
+            bHTML &= "<tr><td>" & smryLoanID & "</td>"
+            bHTML &= "<td>" & smryloanamount & "</td>"
+            bHTML &= "<td>" & smrytotal & "</td>"
+            bHTML &= "<td>" & Difference & "</td>"
+            bHTML &= "<td>" & Accepted & "</td>"
+            bHTML &= "<td>" & Balance & "</td></tr>"
+            bHTML &= vbNewLine
+
+
+        Next i
+
+
+        'msgMessage.Text = "Extract Completed"
+
+
+
+        If errorcount = 0 Then
+            sHTML = "<html><body><head>
+                <style>
+                table {
+                    font-family: arial, sans-serif;
+                    border-collapse: collapse;
+                    width: 100%;
+                }
+
+                td, th {
+                    border: 1px solid #dddddd;
+                    text-align: left;
+                    padding: 8px;
+                }
+
+                tr:nth-child(even) {
+                    background-color: #dddddd;
+                }
+                </style>
+                </head>
+                <table>
+                  <tr>
+                    <th style='font-size:30px' colspan=6>Loan Balances Report - no errors</th>
+                   
+                  </tr>
+                  <tr>
+                    <th>LoanID</th>
+                    <th>LoanAmount</th>
+                    <th>Total</th>
+                    <th>Difference</th>
+                    <th>Accepted</th>
+                    <th>Balance</th>
+                    
+                  </tr>"
+        Else
+            sHTML = "<html><body><head>
+                <style>
+                table {
+                    font-family: arial, sans-serif;
+                    border-collapse: collapse;
+                    width: 100%;
+                }
+
+                td, th {
+                    border: 1px solid #dddddd;
+                    text-align: left;
+                    padding: 8px;
+                }
+
+                tr:nth-child(even) {
+                    background-color: #dddddd;
+                }
+                </style>
+                </head>
+                <table>
+                  <tr>
+                    <th style='font-size:30px' colspan=6>Loan Balances Report - erros encountered</th>
+                   
+                  </tr>
+                  <tr>
+                    <th>LoanID</th>
+                    <th>LoanAmount</th>
+                    <th>Total</th>
+                    <th>Difference</th>
+                    <th>Accepted</th>
+                    <th>Balance</th>
+                    
+                  </tr>"
+        End If
+
+        sHTML &= bHTML
+        sHTML &= "</table></body></html>"
+
+        SendSimpleMail(sUsers, "Loan Balances Report", sHTML)
+
+        ExecuteIT = sHTML
+    End Function
+
+    Sub Main()
+        ExecuteIT()
+    End Sub
+
+
+
+    Public Sub SendSimpleMail(sEmail As String, sSubject As String, sBody As String)
+        Dim sPW As String = ConfigurationManager.AppSettings("FinanceBalancingPW")
+        Dim sUSR As String = ConfigurationManager.AppSettings("FinanceBalancingUSR")
+        Dim MyMailMessage As New MailMessage() With {
+            .From = New MailAddress(sUSR),
+            .Subject = sSubject,
+            .IsBodyHtml = True,
+            .Body = "<table><tr><td>" + sBody + "</table></td></tr>"
+        }
+        MyMailMessage.To.Add(sEmail)
+
+        Dim SMTPServer As New SmtpClient("smtp.office365.com") With {
+            .Credentials = New System.Net.NetworkCredential(sUSR, sPW),
+            .Port = 587,
+            .EnableSsl = True
+        }
+
+        Try
+            SMTPServer.Send(MyMailMessage)
+        Catch ex As Exception
+            SendErrorMessage(ex)
+        End Try
+        SMTPServer = Nothing
+        MyMailMessage = Nothing
+    End Sub
+
+    Sub SendErrorMessage(ByVal ThisException As Exception)
+        Dim errorPW As String = ConfigurationManager.AppSettings("ErrorPW")
+        Dim errorUSR As String = ConfigurationManager.AppSettings("ErrorUSR")
+        Dim mm As New MailMessage() With {
+            .From = New MailAddress(errorUSR),
+            .Subject = "An Error Has Occurred!",
+            .IsBodyHtml = True,
+            .Priority = MailPriority.High
+        }
+        mm.To.Add("web@investandfund.com")
+
+        mm.Body =
+            "<html>" & vbCrLf &
+            "<body>" & vbCrLf &
+            "<h1>An Error Has Occurred!</h1>" & vbCrLf &
+            "<table cellpadding=""5"" cellspacing=""0"" border=""1"">" & vbCrLf &
+            ItemFormat("Time of Error", DateTime.Now.ToString("dd/MM/yyyy HH:mm:sss"))
+
+        Try
+            mm.Body += ItemFormat("Exception Type", ThisException.GetType().ToString())
+        Catch ex As Exception
+            mm.Body += ItemFormat("Exception Type", "Could not get exception type")
+        End Try
+
+        Try
+            mm.Body += ItemFormat("Message", ThisException.Message)
+        Catch ex As Exception
+            mm.Body += ItemFormat("Message", "Could not get message")
+        End Try
+
+        Try
+            mm.Body += ItemFormat("File Name", "DailyReport.vb")
+        Catch ex As Exception
+            mm.Body += ItemFormat("File Name", "Could not get file name")
+        End Try
+
+        Try
+            mm.Body += ItemFormat("Line Number", New StackTrace(ThisException, True).GetFrame(0).GetFileLineNumber)
+        Catch ex As Exception
+            mm.Body += ItemFormat("Line Number", "Could not get line number")
+        End Try
+
+        mm.Body +=
+            "</table>" & vbCrLf &
+            "</body>" & vbCrLf &
+            "</html>"
+
+        Dim smtp As New SmtpClient("smtp.office365.com") With {
+            .Credentials = New System.Net.NetworkCredential(errorUSR, errorPW),
+            .EnableSsl = True,
+            .Port = 587
+        }
+        smtp.Send(mm)
+
+    End Sub
+
+    Public Function ItemFormat(ByVal Title As String, ByVal Message As String) As String
+        Return "  <tr>" & vbCrLf &
+                "  <tdtext-align: right;font-weight: bold"">" & Title & ":</td>" & vbCrLf &
+                "  <td>" & Message & "</td>" & vbCrLf &
+                "  </tr>" & vbCrLf
+    End Function
+End Module
